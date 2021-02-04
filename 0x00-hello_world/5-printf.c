c337e0e558a1f61ade394f00afab1c25b2e83af4
YaelUribe <2962@holbertonschool.com>
2021-02-04 04:06:38 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
 /**
  *main - Entry Point
  *
  *Return: Always 0 (succes)
  */
-int main (void
-	  {
-		  char puzz[55]= "with proper grammar, but the outcome is a piece of art,";
-		  printf("%s\n, puzz");
-		  return (0);
+int main(void)
+{
+char puzz[55] = "with proper grammar, but the outcome is a piece of art,";
+printf("%s\n", puzz);
+return (0);
 }
