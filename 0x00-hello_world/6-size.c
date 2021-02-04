e8a77123eb2d783dc4c36be5401efb28e8405a70
YaelUribe <2962@holbertonschool.com>
2021-02-04 04:18:56 +0000

    SIZE

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
+	printf("Size of double: %d byte(s)\n", sizeof(double));
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+}
