5b1182a801fee00164335c03cd5da2d1066e4451
YaelUribe <2962@holbertonschool.com>
2021-02-04 03:52:34 +0000

    3,4,5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - Entry Point
+ *
+ *Return: Always 0 (succes)
+ */
+int main (void
+	  {
+		  char puzz[55]= "with proper grammar, but the outcome is a piece of art,";
+		  printf("%s\n, puzz");
+		  return (0);
+}
