0973cd8d5e1cbc974d0ee5a7c59f5aad298745f8
YaelUribe <2962@holbertonschool.com>
2021-02-05 20:10:53 +0000

    38-48

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 38; n < 48; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
