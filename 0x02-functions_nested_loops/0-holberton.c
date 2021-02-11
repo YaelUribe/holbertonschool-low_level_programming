bec4fb4ec21727935d461856675cfd44fa66e862
Yaeluribe <2962@holbertonschool.com>
2021-02-10 19:03:32 -0500

    add

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
  *main - Entry ponit
  *
  *Return: Always 0 (Success)
  */
 int main(void)
-{unsingned int x;
-	char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
-
-	for (x = 0; x < sizeof(holberton); x++)
-	{
-		_putchar(holberton[x]);
-	}
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
