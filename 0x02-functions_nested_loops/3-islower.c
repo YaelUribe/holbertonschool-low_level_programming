92c8dc4d2ddf3239e861e92ac00066bcc7b78507
Yaeluribe <2962@holbertonschool.com>
2021-02-10 21:14:08 -0500

    lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks Lowercase.
+ *@c: checks lowercase
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{	return (1);
+			}
+	else
+	{
+		return (0);
+	}
+		}
