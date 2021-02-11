f86bec681db8d6e321a981bdca1d02470b77f370
Yaeluribe <2962@holbertonschool.com>
2021-02-11 14:07:19 -0500

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *print_last_digit - Prints last digit of any number
+ *
+ *@r: modulo
+ *
+ *Return: Last digit of r
+ */
+int print_last_digit(int r)
+{
+	int last_digit;
+
+	last_digit = r % 10;
+	if (last_digit < 0)
+	{
+		last_digit *= -1;
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+	else
+	{
+		_putchar(last_digit + '0');
+		return (0);
+	}
+}
