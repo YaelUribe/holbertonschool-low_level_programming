b02a6c0988480872c2e4cdf2d41a26cf33b8ee94
Yaeluribe <2962@holbertonschool.com>
2021-02-10 23:46:56 -0500

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *print_alphabet - Printing the alphabet
+ *
+ *Return: Always 0 (success)
+ */
+int _abs(int r)
+{
+	if (r > 0)
+	{
+	return (r);
+	}
+	else if (r < 0)
+	{
+	return (r * -1);
+	 }
+	else
+	return (0);
+}
