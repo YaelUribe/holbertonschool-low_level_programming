8c4e958852cf19773c69584bab69ae323b8c692f
Yaeluribe <2962@holbertonschool.com>
2021-02-12 11:14:10 -0500

    prntlst

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,13 +14,11 @@ int print_last_digit(int r)
 	last_digit = r % 10;
 	if (last_digit < 0)
 	{
-		last_digit = -last_digit;
-		_putchar(last_digit + '0');
-		return (last_digit);
+		last_digit = last_digit * -1;
 	}
-	else
-	{
-		_putchar(last_digit + '0');
-		return (0);
-	}
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+
 }
