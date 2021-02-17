dec7b4256605750918e52fa60c9568844fb4742f
Yaeluribe <2962@holbertonschool.com>
2021-02-16 19:47:19 -0500

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ *more_numbers - Prints more numbers
+ *
+ *Return: void
+ *
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+	int k;
+	int l;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			k = y;
+			if (y > 9)
+			{
+				k = y / 10;
+				l = y % 10;
+			}
+			_putchar(k + 48);
+			if (y > 9)
+			{
+				_putchar(l + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
