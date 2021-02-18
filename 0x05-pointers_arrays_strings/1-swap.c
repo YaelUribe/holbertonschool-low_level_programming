82328973619164f54485329c3c7d0ee50b64094c
Yaeluribe <2962@holbertonschool.com>
2021-02-18 00:04:56 -0500

    swapping

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *print_square - Prints the size of a square
+ *@a: 98
+ *@b: 42
+ *Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int R;
+
+	R = *a;
+	*a = *b;
+	*b = R;
+}
