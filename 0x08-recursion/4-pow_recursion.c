9ae14c3de5318340b1b695d57e8109b80866c772
Yaeluribe <2962@holbertonschool.com>
2021-02-24 18:22:51 -0500

    x to y power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *_pow_recursion - returns the value of x^y
+ *@x: value
+ *@y: power for x
+ *
+ *Return: returns the value of x^y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (x == 1 || y == 0)
+	{
+		return (1);
+	}
+		if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
