8f69eefee4f70f00a3f5289e7dd63b89aa2f9c9e
Yaeluribe <2962@holbertonschool.com>
2021-03-04 15:22:43 -0500

    allocating memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,21 +10,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	unsigned int r, t;
-	int *v;
+	unsigned int *v;
 
-	for (r = 0; r <= b; r++)
-	{
-		;
-	}
-	v = malloc(sizeof(unsigned int) * r + 1);
+	v = (void *) malloc(b);
 	if (v == NULL)
 	{
 		return ("98");
-	}
-	for (t = 0; t <= b; t++)
-	{
-		;
 	}
 	return (v);
 }
