8b87e2a0fffd61b8d655ffc7dd343d80f8019c9b
Yaeluribe <2962@holbertonschool.com>
2021-03-08 22:28:32 -0500

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ *free_dog - To free dogs
+ *@d: pointer to dog
+ *Return void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
