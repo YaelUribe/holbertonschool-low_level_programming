a9788ac601cb512e5d0b8560e7376a3862be920a
Yaeluribe <2962@holbertonschool.com>
2021-03-09 11:54:15 -0500

    nudog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
  *strln - string lenght
- *@f:contains a character array
- *Return Lenght of the string
+ *@s:contains a character array
+ *Return: Lenght of the string
  */
 int strln(char *s)
 {
 	int y;
 
 	for (y = 0; s[y] != '\0'; y++)
-		return (y);
+	{
+		;
+	}
+	return (y);
 }
+
 /**
  *stringcpy - function that copies the string pointed to by par
  *@destination: Parameter
  *@par: Parameter
- *return: destination
+ *Return: destination
  */
 char *stringcpy(char *destination, char *par)
 {
@@ -36,5 +41,38 @@ char *stringcpy(char *destination, char *par)
  *@name: Dog's Name
  *@age: Dog's age
  *@owner: Dog's owner
+ *Return: new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
+{
+	int r, f;
+	char *w, *t;
+	struct dog *nudog;
+
+	nudog = malloc(sizeof(struct dog));
+	if (nudog == NULL)
+	{
+		return (NULL);
+	}
+	r = strln(name);
+	f = strln(owner);
+	w = malloc(r + 1);
+	if (w == NULL)
+	{
+		free(nudog);
+		return (NULL);
+	}
+	t = malloc(f + 1);
+	if (t == NULL)
+	{
+		free(nudog);
+		free(w);
+		return (NULL);
+	}
+	w = stringcpy(w, name);
+	t = stringcpy(t, owner);
+	nudog->name = w;
+	nudog->age = age;
+	nudog->owner = t;
+	return (nudog);
+}
