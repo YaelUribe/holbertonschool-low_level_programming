9c8e2d6c2c7dca7c78d45dbb0a9b700ff1e3cc7c
Yaeluribe <2962@holbertonschool.com>
2021-03-10 18:23:15 -0500

    integer index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+/**
+ *int_index - function to search an integer
+ *@array: array to evaluate
+ *@size: size of the array
+ *@cmp: pointer to function  to compare values
+ *Return: -1 if fails index of integer if success
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int w;
+
+	if (cmp == NULL || array == NULL)
+	{
+		return (0);
+	}
+	for (w = 0; w < size; w++)
+	{
+		if (size <= 0)
+		{
+			return (-1);
+		}
+		else if ((cmp(array[w])) != 0)
+		{
+			return (w);
+		}		
+	}
+	return (-1);
+}
