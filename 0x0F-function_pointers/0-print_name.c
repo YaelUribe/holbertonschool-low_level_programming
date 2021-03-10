6aae53b637b974e546e539f14398352e90d5a615
Yaeluribe <2962@holbertonschool.com>
2021-03-10 16:49:49 -0500

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ *print_name - function that prints a name
+ *@name: pointer to a string
+ *@f: Pointer
+ *Return: Always 0 when success
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
