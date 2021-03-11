b5936d95b01aa66d9d3032daa497d082898e1adc
Yaeluribe <2962@holbertonschool.com>
2021-03-10 20:15:05 -0500

    get proper function

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "calc.h"
+/**
+ *get_op_func - Selects the correct function to perform operation
+ *@s: operator passed as argument
+ *Return: adecuated function for operation
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
