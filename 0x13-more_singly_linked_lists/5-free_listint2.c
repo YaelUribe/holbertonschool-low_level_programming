87f4ef06a87f94b7aa71d258ce51afc243afcbe8
Yaeluribe <2962@holbertonschool.com>
2021-03-24 14:01:18 -0500

    Function to free a linked list that uses double pointers

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *free_listint2 - functiont to Free the list
+ *@head: Head node
+ *Return: Void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *copy;
+
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		copy = *head;
+		*head = (*head)->next;
+		free(copy);
+	}
+}
