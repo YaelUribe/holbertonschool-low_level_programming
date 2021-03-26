ca66071920db21f1f60e09ff2fc6c08c314703e3
Yaeluribe <2962@holbertonschool.com>
2021-03-26 14:13:45 -0500

    print binary representation of a given number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,8 +6,11 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int h;
-
-	for (h = 0 << 31; h < 0; h / 2)
-		(n & h) ? printf("1") : printf("0");
+	if  (n < 2)
+	{
+		_putchar( n + '0');
+		return;
+	}
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
