603889868351f0155fd0f623d3593e360925a194
Yaeluribe <2962@holbertonschool.com>
2021-03-26 14:36:09 -0500

    get the bit from a specific index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
  *get_bit - function to return  bit value from a given index
- *@n: 
- *@index: 
+ *@n: bit to compare
+ *@index: bit given
  *Return: index if success or -1 if fails
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > (sizeof(long unsigned int) * 8))
+	if (index > (sizeof(unsigned long int) * 8))
 		return (-1);
 	n >>= index;
 	if ((n & 1) == 1)
