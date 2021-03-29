0de7a413f497a13781142d06c4b6daadd640d08a
Yaeluribe <2962@holbertonschool.com>
2021-03-29 16:47:16 -0500

    reading a file and printing amount of chars

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+*read_textfile - function to read a text file
+*@filename: the file to read
+*@letters: amount of letters to read and print
+*Return: the actual number of letters or 0 if it fails
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;/*creating the file*/
+	int *bffr; /*to separate the memory of letters*/
+	int length;/*to use read with bffr and letters*/
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);/*opening our file*/
+	if (fd == -1)
+	{
+		return (0);
+		exit(1);
+	}
+	bffr = malloc(sizeof(char) * letters);/*assign a buffer*/
+	if (bffr == NULL)
+		return (0);
+	length = read(fd, bffr, letters); /*file, bffer, letters*/
+	if (write(1, bffr, length) != length)
+		return (0);
+	close(fd);
+	return (length);
+}
