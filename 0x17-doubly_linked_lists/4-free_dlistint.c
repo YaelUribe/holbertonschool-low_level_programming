7ad42b000ec376a25fe9a42a9bb14da19d4d36f7
Yaeluribe <2962@holbertonschool.com>
2021-04-30 19:04:58 -0500

    function to free memory

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - function to free mallocs
+ * @head: position to free
+ * Return - void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *copy;
+
+	while (head != NULL)
+	{
+		copy = head->next;
+		free(head);
+		head = copy;
+	}
+}
