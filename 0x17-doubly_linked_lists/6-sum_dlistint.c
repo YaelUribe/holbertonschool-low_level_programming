c4609e66be7a02a6cc9dfc8fb6aee11af188e584
Yaeluribe <2962@holbertonschool.com>
2021-04-30 19:44:58 -0500

    Function to add all data stored in a list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *sum_dlistint - function to sum all data stored in a list
+ *@head: Pointer to the 1st element in a list
+ *Return: the sum of all data stored in the list
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *copy = NULL;
+	int i = 0;
+
+	copy = head;
+	if (copy == NULL)
+		return (0);
+	while (copy->next != NULL)
+	{
+		copy = copy->next;
+		i += copy->n;
+	}
+	return (i);
+}
