816b377b109e95e80e7f7d4667da0e3cc6473d84
Yaeluribe <2962@holbertonschool.com>
2021-05-02 22:30:19 -0500

    corrected structure and syntax

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,22 @@
 #include "lists.h"
 /**
+* dlistint_len - function to determine ammount of elements in a list
+* @h: Pointer to the first element of the list
+* Return: The number of elements ot he list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+	const dlistint_t *copy = NULL;
+
+	copy = h;
+	for (i = 0; copy != NULL; i++)
+	{
+		copy = copy->next;
+	}
+	return (i);
+}
+/**
  * insert_dnodeint_at_index - Function to insert a new node in a given index
  * @h:Pointer to the first element in a list
  * @idx:Index position to insert new node
@@ -9,27 +26,28 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *copy = NULL, *nu_node = NULL;
-	unsigned int i;
+	unsigned int i, length = 0;
 
-	nu_node = malloc(sizeof(dlistint_t));
 	copy = *h;
-	while (copy->next != NULL)
+	length = dlistint_len(copy);
+	nu_node = malloc(sizeof(dlistint_t));
+	if (!nu_node)
+		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	if (idx == length)
+		return (add_dnodeint_end(h, n));
+	while (i == idx - 1)
 	{
-		if (i == idx - 1)
-		{
-			copy->next = nu_node;
-			nu_node->prev = copy;
-			i++;
-		}
-		if (i == idx)
-		{
-			copy->prev = nu_node;
-			nu_node->n = n;
-			nu_node->next = copy;
-			return (nu_node);
-		}
 		copy = copy->next;
+		if (!copy)
+			return (NULL);
 		i++;
 	}
-	return (0);
+	nu_node->n = n;
+	nu_node->next = copy->next;
+	copy->next->prev = nu_node;
+	copy->next = nu_node;
+	nu_node->prev = copy;
+	return (nu_node);
 }
