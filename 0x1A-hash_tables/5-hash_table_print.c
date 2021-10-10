8ccd1fb2be85136df13ddbfd44affbeeb178b552
Yaeluribe <2962@holbertonschool.com>
2021-10-09 22:44:02 -0500

    testing

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,6 +3,19 @@
 /**
  * hash_table_print - Function to print a table
  * @ht: Hash table to print
- * 
+ * void function
  */
 void hash_table_print(const hash_table_t *ht)
+{
+	int i;
+	hash_node_t *copier = NULL;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		copier = ht->array[i];
+		if (copier == NULL)
+			continue;
+		else
+			printf("%s: %s", copier->key, copier->value);
+	}
+}
