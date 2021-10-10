b37a3e4643749d478f15e4c6c69c3473b818aa94
Yaeluribe <2962@holbertonschool.com>
2021-10-09 21:03:28 -0500

    Set hash table after using duplicates of value and key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,20 +13,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 
 	unsigned long int index = 0;
+	char *dkey = NULL, *dvalue = NULL;
 	hash_node_t *nu_node = NULL;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
+	dkey = strdup(key);
+	dvalue = strdup(value);
 	index = key_index((unsigned char *)key, ht->size);
 	nu_node = ht->array[index];
-/*With this in case the spot is already taken/if info and dkey is the same,
-link it to the end like a linked list*/
+/*
+*With this in case the spot is already taken/if info and dkey is the same,
+* link it to the end like a linked list
+*/
 	while (nu_node)
 	{
 		if (strcmp(nu_node->key, key) == 0)
 		{
-			nu_node->value = value;
+			nu_node->value = dvalue;
 			return (1);
 		}
 		nu_node = nu_node->next;
@@ -35,8 +40,8 @@ link it to the end like a linked list*/
 	nu_node = malloc(sizeof(hash_node_t));
 	if (nu_node == NULL)
 		return (0);
-	nu_node->key = key;
-	nu_node->value = value;
+	nu_node->key = dkey;
+	nu_node->value = dvalue;
 	nu_node->next = ht->array[index];
 	ht->array[index] = nu_node;
 	return (1);
