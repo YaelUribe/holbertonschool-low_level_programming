f010d60b3561a7e2bad13c3e77e43cfd6cfd6074
YaelUribe <yael3x@hotmail.com>
2022-01-18 19:30:04 -0500

    modified file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,10 +1,8 @@
-#ifndef SEACH_ALGOS
-#define SEACH_ALGOS
+#ifndef _SEACH_ALGOS_
+#define _SEACH_ALGOS_
 #include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
 #include <stdlib.h>
-#include <string.h>
+
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
