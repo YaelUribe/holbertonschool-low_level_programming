f742753f92531a320e1be9941205941a61383eba
YaelUribe <yael3x@hotmail.com>
2022-01-18 19:25:53 -0500

    Task 0, linear search algorithm

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEACH_ALGOS
+#define SEACH_ALGOS
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
